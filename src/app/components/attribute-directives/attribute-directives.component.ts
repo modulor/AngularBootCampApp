3ea90dbb39f693015abc1b59198daf3aca8ddab6
Mario Lopez <Mario_Lopez2@oxy.com>
2024-04-02 15:11:04 -0600

    attribute directives componente generated

---
+++ src/app/components/attribute-directives/attribute-directives.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-directives',
+  templateUrl: './attribute-directives.component.html',
+  styleUrl: './attribute-directives.component.scss'
+})
+export class AttributeDirectivesComponent {
+
+}
