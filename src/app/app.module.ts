a6352fc507f205fce2bf601a4a89916173274a8c
Mario Lopez <Mario_Lopez2@oxy.com>
2024-04-04 15:04:52 -0600

    structutal-directives component generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AttributeDirectivesComponent } from './components/attribute-directives/attribute-directives.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StructuralDirectivesComponent } from './components/structural-directives/structural-directives.component';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent, AttributeDirectivesComponent],
+  declarations: [AppComponent, AttributeDirectivesComponent, StructuralDirectivesComponent],
   imports: [AppRoutingModule, BrowserModule],
   providers: [],
 })
