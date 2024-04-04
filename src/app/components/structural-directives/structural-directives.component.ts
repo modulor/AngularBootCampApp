8e965b51eefda2aa0bcba8d7926fb23c5d20b8d4
Mario Lopez <Mario_Lopez2@oxy.com>
2024-04-04 15:17:19 -0600

    ngFor, ngIf, ngSwitch

--- src/app/components/structural-directives/structural-directives.component.ts
+++ src/app/components/structural-directives/structural-directives.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-structural-directives',
   templateUrl: './structural-directives.component.html',
-  styleUrl: './structural-directives.component.scss'
+  styleUrl: './structural-directives.component.scss',
 })
 export class StructuralDirectivesComponent {
-
+  public letters: Array<string> = ['A', 'B', 'C'];
 }
