6e6b234a0b2eb34ae0b8fd56bbbc3df9dd65a58b
Mario Lopez <Mario_Lopez2@oxy.com>
2024-04-10 15:50:54 -0600

    homework 4 - data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AttributeDirectivesComponent } from './components/attribute-directives/attribute-directives.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { StructuralDirectivesComponent } from './components/structural-directives/structural-directives.component';
 import { CapitalizeDirective } from './directives/capitalize.directive';
 import { CustomDirectivesComponent } from './components/custom-directives/custom-directives.component';
+import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { StructuralDirectivesComponent } from './components/structural-directives/structural-directives.component';
 
 @NgModule({
   bootstrap: [AppComponent],
-  declarations: [AppComponent, AttributeDirectivesComponent, StructuralDirectivesComponent, CapitalizeDirective, CustomDirectivesComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  declarations: [
+    AppComponent,
+    AttributeDirectivesComponent,
+    StructuralDirectivesComponent,
+    CapitalizeDirective,
+    CustomDirectivesComponent,
+    DataBindingComponent,
+  ],
+  imports: [AppRoutingModule, BrowserModule, FormsModule],
   providers: [],
 })
 export class AppModule {}
